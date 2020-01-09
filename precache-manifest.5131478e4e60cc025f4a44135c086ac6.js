476e69fc4dc8a36d24cd96211d3dbd54f5b77ac1
shamseena-dev <shamseenakarumarot@gmail.com>
2020-01-10 00:45:35 +0100

    Updates

---
+++ precache-manifest.5131478e4e60cc025f4a44135c086ac6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ccf274ba0d2991494dfa4ced8d8615",
+    "url": "/News-ReactApp/index.html"
+  },
+  {
+    "revision": "f24938472ece4e40d377",
+    "url": "/News-ReactApp/static/css/main.e846a8a1.chunk.css"
+  },
+  {
+    "revision": "6ae9741286f8e40ae5a5",
+    "url": "/News-ReactApp/static/js/2.bf49a0b8.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/News-ReactApp/static/js/2.bf49a0b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f24938472ece4e40d377",
+    "url": "/News-ReactApp/static/js/main.118f7b2a.chunk.js"
+  },
+  {
+    "revision": "e2be147c15658a547748",
+    "url": "/News-ReactApp/static/js/runtime-main.105895d1.js"
+  },
+  {
+    "revision": "4cdf4bc31b4fb62970dc877d66131c47",
+    "url": "/News-ReactApp/static/media/logo1.4cdf4bc3.svg"
+  },
+  {
+    "revision": "c9ce145e8f98b5388b77f0eabdfd5574",
+    "url": "/News-ReactApp/static/media/planet.c9ce145e.jpg"
+  }
+]);
