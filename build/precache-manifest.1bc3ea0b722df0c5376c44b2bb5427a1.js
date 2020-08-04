5d1b2a0e1dc271cca9c72d77dae7fb94c15767bc
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-08-04 18:51:07 +0200

    fix build issue with netlify

---
+++ build/precache-manifest.1bc3ea0b722df0c5376c44b2bb5427a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bc5fbd2d4e0116a0ceb9344ce4978b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6195c0b6100fdc4aff37",
+    "url": "/static/css/main.32e003e6.chunk.css"
+  },
+  {
+    "revision": "85f0dbe06926d83b23be",
+    "url": "/static/js/2.db4f2567.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.db4f2567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6195c0b6100fdc4aff37",
+    "url": "/static/js/main.4d0fda7e.chunk.js"
+  },
+  {
+    "revision": "c48be26b3d9edd41ccc5",
+    "url": "/static/js/runtime-main.714d63fe.js"
+  },
+  {
+    "revision": "11ec11985387f99ba176eb60ee9452b0",
+    "url": "/static/media/news.11ec1198.jpg"
+  }
+]);
